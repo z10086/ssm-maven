4b9131a68a525a3d0b0e7964318891248808616d
zcq <z.github@gmail.com>
2017-06-29 14:57:08 +0800

    添加webSocket支持

--- src/main/java/com/z/test/controller/shiroTestController.java
+++ src/main/java/com/z/test/controller/shiroTestController.java
@@ -5,28 +5,18 @@ package com.z.test.controller;
  */
 
 
-import com.z.test.pojo.User;
-import com.z.test.service.IUserService;
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
-import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.ehcache.EhCacheCacheManager;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpSession;
-
 
 @RestController
 @RequestMapping("/shiro")
-public class shiroTestController {
-    static private Logger logger = LoggerFactory.getLogger(shiroTestController.class);
+public class ShiroTestController {
+    static private Logger logger = LoggerFactory.getLogger(ShiroTestController.class);
 
 
 
